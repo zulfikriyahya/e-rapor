6e76ea112ad65fcbc2b3e978e8527d30c911c2b6
zulfikriyahya <zulfikriyahya18@gmail.com>
2024-12-01 16:09:38 +0700

    Initial Project.

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'nama' => 'Super Admin',
+            'email' => 'super@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'role' => 'superadmin',
+            'avatar' => '',
+        ]);
+    }
+}
